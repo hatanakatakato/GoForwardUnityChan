7af8ad137120b6f84690ca955c600b3fe00faddb
hatanakatakato <80793900+hatanakatakato@users.noreply.github.com>
2021-08-23 08:19:13 +0900

    cube生成完了
    cubeを自動生成実行

---
+++ Assets/CubeController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    // キューブの移動速度
+    private float speed = -12;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // キューブを移動させる
+        // Time.deltaTimeは前フレームからの経過時間を返す
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
